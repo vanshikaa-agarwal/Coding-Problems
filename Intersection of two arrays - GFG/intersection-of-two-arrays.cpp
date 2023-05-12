7e913d2539713e65b7ea512b2a521dffedd49384
Vanshika agarwal <97115082+vanshikaa-agarwal@users.noreply.github.com>
2023-05-12 13:28:17 +0530

    Added solution - LeetHub

---
+++ Intersection of two arrays - GFG/intersection-of-two-arrays.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    // Function to return the count of the number of elements in
+    // the intersection of two arrays.
+    int NumberofElementsInIntersection(int a[], int b[], int n, int m) {
+        // Your code goes here
+        set<int>s1,s2;
+        for(int i=0;i<n;i++){
+            s1.insert(a[i]);
+        }
+        for(int i=0;i<m;i++){
+            s2.insert(b[i]);
+        }
+        int ans=0;
+        for(auto it:s1){
+            
+            if(s2.find(it)!=s2.end()){
+                ans++;
+            }
+            
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        int a[n], b[m];
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        for (int i = 0; i < m; i++) cin >> b[i];
+        Solution ob;
+        cout << ob.NumberofElementsInIntersection(a, b, n, m) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
