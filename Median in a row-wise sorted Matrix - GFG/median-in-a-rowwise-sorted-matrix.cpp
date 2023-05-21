befe3d4e7dcbdaa49b63781ce2b40a3fc6ebfaa9
Vanshika agarwal <97115082+vanshikaa-agarwal@users.noreply.github.com>
2023-05-21 15:40:07 +0530

    Added solution - LeetHub

--- Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.cpp
+++ Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.cpp
@@ -11,34 +11,33 @@ using namespace std;
 class Solution{   
 public:
     int median(vector<vector<int>> &matrix, int R, int C){
-        // code here       
+        int med_pos=(R*C+1)/2;
         int minv=INT_MAX;
         int maxv=INT_MIN;
         for(int i=0;i<R;i++){
-            minv=min(minv,matrix[i][0]);
-            maxv=max(maxv,matrix[i][C-1]);
+            minv=min(matrix[i][0],minv);
+            maxv=max(matrix[i][C-1],maxv);
         }
-        int med_pos=(R*C+1)/2;
         int l=minv;
         int h=maxv;
         
         while(h>l){
+           
             int mid=l+(h-l)/2;
-            int ct=0;
+            int val=0;
             for(int i=0;i<R;i++){
-                int val=upper_bound(matrix[i].begin(),matrix[i].end(),mid)-matrix[i].begin();
-                ct+=val;
+                 auto it=upper_bound(matrix[i].begin(),matrix[i].end(),mid)-matrix[i].begin();
+                val+=it;
             }
-            if(ct<med_pos){
-                    l=mid+1;
-                }
-            else{
-                    h=mid;
-                }
-                
-                
+            if(val<med_pos){
+            l=mid+1;
+            }
+            else if(val>=med_pos)
+            h=mid;
+           
         }
-    return l;  
+        
+        return l;
     }
     
 };
